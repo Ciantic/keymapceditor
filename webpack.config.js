374c072b1805621e61da25b8ecf57a938465adfc
Jari Pennanen <ciantic@oksidi.com>
2017-06-14 23:11:45 +0300

    ...

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+var path = require("path");
+var packageJson = require(path.join(__dirname, 'package.json'));
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var appDir = path.resolve(__dirname, "src");
+var config = {
+    entry: {
+        vendors: [
+        ].concat(Object.keys(packageJson.dependencies)),
+        app: [
+            path.join(__dirname, 'src', 'Index.tsx')
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.less', '.css']
+    },
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: '[name].js',
+        publicPath: ""
+    },
+    module: {
+        loaders: [
+            { test: /\.tsx?$/, loaders: ['ts-loader'], include: appDir },
+            { test: /\.css$/, exclude: /\.import\.css$/, loader: "style-loader!css", include: appDir },
+            { test: /\.scss$/, exclude: /\.module\.scss$/, loader: "style-loader!css-loader!postcss-loader!sass-loader", include: appDir },
+            { test: /\.module\.scss$/, loader: "style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!sass-loader", include: appDir },
+            { test: /\.svg$/, loader: 'svg-inline-loader' },
+            {
+                test: /\.(woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    name: 'fonts/[hash].[ext]',
+                    limit: 5000,
+                    mimetype: 'application/font-woff',
+                },
+            },{
+                test: /\.(ttf|eot)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[hash].[ext]',
+                },
+            },
+            { test: /\.(jpg|png|gif)$/, loader: "file-loader?name=[name].[ext]", include: appDir }
+        ]
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"production"'
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            filename: "index.html",
+            excludeChunks: ["o2c"]
+        }),
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendors', filename: `vendors.js` }),
+        new ExtractTextPlugin({ filename: `[name].css`, allChunks: true })
+    ]
+};
+
+module.exports = config;
